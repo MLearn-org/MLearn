854598d6ac0b8997bb0e503a798a1a2e61ec7c5c
SL-Pirate <isiraherath626@gmail.com>
2023-03-07 10:04:07 +0530

    Fixed splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -20,14 +20,15 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Container(
         width: double.infinity,
         height: double.infinity,
+        padding: const EdgeInsets.all(100),
         color: Colors.white,
-        // child: Image.asset("gfx/icon.png")
+        child: Image.asset("res/gfx/logo.png")
       )
     );
   }
 
   void goHome(BuildContext context){
-    Future.delayed(const Duration(seconds: 2), (){
+    Future.delayed(const Duration(seconds: 3), (){
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginPage()));
     });
   }
